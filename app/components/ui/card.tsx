ca4faf56e807d65056d33b583f8d17acdd3b58c6
ruri-nagao <ruri.nagao.1018@gmail.com>
2025-05-01 18:52:49 +0900

    カードコンポーネント作成

---
+++ app/components/ui/card.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  title: string;
+  content: string;
+  button?: React.ReactNode;
+  style?: string;
+};
+
+export const Card = ({title, content, button, style}: Props) => {
+  const textColor = style ? style : 'text-white';
+  return (
+    <div className="card bg-neutral text-neutral-content w-auto">
+      <div className="card-body items-center text-center">
+        <h2 className={`card-title ${textColor}`}>{title}</h2>
+        <p className={textColor}>{content}</p>
+        <div className="card-actions justify-center mt-3">{button}</div>
+      </div>
+    </div>
+  );
+};
